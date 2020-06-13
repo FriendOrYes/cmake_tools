fdddefda85e51311d169a629cf59ce3587551fdd
a.chaban <a.chaban@infomir.com>
2020-06-14 01:36:16 +0300

    [pgcmake] added cmake_parse_argument support

--- cmake/PGMakefileGenerator.cmake
+++ cmake/PGMakefileGenerator.cmake
@@ -1,22 +1,30 @@
 function(read_makefile_template)
     file(READ ${PGMakefileTemplate} PG_MAKEFILE)
 
-    string(REPLACE "<modulename>" "${modulename}" PG_MAKEFILE ${PG_MAKEFILE})
-    string(REPLACE "<targer_source_list>" "${targer_source_list}" PG_MAKEFILE ${PG_MAKEFILE})
-    string(REPLACE "<shared_libs>" "${shared_libs}" PG_MAKEFILE ${PG_MAKEFILE})
-    string(REPLACE "<cpp_flags>" "${cpp_flags}" PG_MAKEFILE ${PG_MAKEFILE})
+    string(REPLACE "<modulename>" "${PG_MAKEFILE_MODULENAME}" PG_MAKEFILE ${PG_MAKEFILE})
+    string(REPLACE "<targer_source_list>" "${PG_MAKEFILE_TARGET_SOURCE_LIST}" PG_MAKEFILE ${PG_MAKEFILE})
+    string(REPLACE "<shared_libs>" "${PG_MAKEFILE_SHARED_LIBS}" PG_MAKEFILE ${PG_MAKEFILE})
+    string(REPLACE "<cpp_flags>" "${PG_MAKEFILE_CPP_FLAGS}" PG_MAKEFILE ${PG_MAKEFILE})
 
-    if ( IS_DIRECTORY ${dir_out} )
-        message(dir_out "=${dir_out}")
-        set(makefile_generated ${dir_out}/${modulename}_Makefile PARENT_SCOPE)
+    if ( IS_DIRECTORY ${PG_MAKEFILE_DIR_OUT} )
+        message(PG_MAKEFILE_DIR_OUT "=${PG_MAKEFILE_DIR_OUT}")
+        set(makefile_generated ${PG_MAKEFILE_DIR_OUT}/${PG_MAKEFILE_MODULENAME}_Makefile PARENT_SCOPE)
     else()
-        set(makefile_generated ${modulename}_Makefile PARENT_SCOPE)
+        set(makefile_generated ${PG_MAKEFILE_MODULENAME}_Makefile PARENT_SCOPE)
     endif()
 
     set(PG_MAKEFILE_OUT ${PG_MAKEFILE} PARENT_SCOPE)
 endfunction()
 
-function(pg_makefile_plugin_generate modulename targer_source_list shared_libs cpp_flags dir_out)
+function(pg_makefile_plugin_generate)
+
+    cmake_parse_arguments(
+        PG_MAKEFILE
+        ""
+        "MODULENAME;DIR_OUT"
+        "TARGET_SOURCE_LIST;SHARED_LIBS;CPP_FLAGS"
+        ${ARGN}
+    )
 
     unset(PG_MAKEFILE_OUT)
 
@@ -26,24 +34,23 @@ function(pg_makefile_plugin_generate modulename targer_source_list shared_libs c
 
 endfunction()
 
-function(pg_makefile_extention_generate modulename targer_source_list shared_libs cpp_flags version dir_out)
+function(pg_makefile_extention_generate)
+
+    cmake_parse_arguments(
+        PG_MAKEFILE
+        ""
+        "MODULENAME;VERSION;DIR_OUT"
+        "TARGET_SOURCE_LIST;SHARED_LIBS;CPP_FLAGS"
+        ${ARGN}
+    )
 
     unset(PG_MAKEFILE_OUT)
 
     read_makefile_template()
 
-    file(WRITE ${makefile_generated} "EXTENSION = ${modulename}\n")
-    file(APPEND ${makefile_generated} "DATA = ${modulename}--${version}.sql\n")
+    file(WRITE ${makefile_generated} "EXTENSION = ${PG_MAKEFILE_MODULENAME}\n")
+    file(APPEND ${makefile_generated} "DATA = ${PG_MAKEFILE_MODULENAME}--${PG_MAKEFILE_VERSION}.sql\n")
 
     file(APPEND ${makefile_generated} ${PG_MAKEFILE_OUT})
 
 endfunction()
-
-
-    #cmake_parse_arguments(
-    #    COMPLEX_PREFIX
-    #    "SINGLE;ANOTHER"
-    #    "ONE_VALUE;ALSO_ONE_VALUE"
-    #    "MULTI_VALUES"
-    #    ${ARGN}
-    #)
