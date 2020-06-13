8fd12462997afa21c036b23666d36285097f104d
a.chaban <a.chaban@infomir.com>
2020-06-13 08:31:58 +0300

    [pgcmake] separete function for extention and plugin

--- cmake/PGMakefileGenerator.cmake
+++ cmake/PGMakefileGenerator.cmake
@@ -1,5 +1,4 @@
-function(pg_makefile_generate modulename targer_source_list shared_libs cpp_flags dir_out)
-
+function(read_makefile_template)
     file(READ ${PGMakefileTemplate} PG_MAKEFILE)
 
     string(REPLACE "<modulename>" "${modulename}" PG_MAKEFILE ${PG_MAKEFILE})
@@ -9,9 +8,42 @@ function(pg_makefile_generate modulename targer_source_list shared_libs cpp_flag
 
     if ( IS_DIRECTORY ${dir_out} )
         message(dir_out "=${dir_out}")
-        file(WRITE ${dir_out}/${modulename}_Makefile ${PG_MAKEFILE})
+        set(makefile_generated ${dir_out}/${modulename}_Makefile PARENT_SCOPE)
     else()
-        file(WRITE ${modulename}_Makefile ${PG_MAKEFILE})
+        set(makefile_generated ${modulename}_Makefile PARENT_SCOPE)
     endif()
 
+    set(PG_MAKEFILE_OUT ${PG_MAKEFILE} PARENT_SCOPE)
 endfunction()
+
+function(pg_makefile_plugin_generate modulename targer_source_list shared_libs cpp_flags dir_out)
+
+    unset(PG_MAKEFILE_OUT)
+
+    read_makefile_template()
+
+    file(WRITE ${makefile_generated} ${PG_MAKEFILE_OUT})
+
+endfunction()
+
+function(pg_makefile_extention_generate modulename targer_source_list shared_libs cpp_flags version dir_out)
+
+    unset(PG_MAKEFILE_OUT)
+
+    read_makefile_template()
+
+    file(WRITE ${makefile_generated} "EXTENSION = ${modulename}\n")
+    file(APPEND ${makefile_generated} "DATA = ${modulename}--${version}.sql\n")
+
+    file(APPEND ${makefile_generated} ${PG_MAKEFILE_OUT})
+
+endfunction()
+
+
+    #cmake_parse_arguments(
+    #    COMPLEX_PREFIX
+    #    "SINGLE;ANOTHER"
+    #    "ONE_VALUE;ALSO_ONE_VALUE"
+    #    "MULTI_VALUES"
+    #    ${ARGN}
+    #)
